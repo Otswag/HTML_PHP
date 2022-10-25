f3af8d4fcd08c1a0200fcda72fae82082c7b0a53
Otswag <106761383+Otswag@users.noreply.github.com>
2022-10-25 14:36:05 +0100

    created

--- php/logout.php
+++ php/logout.php
@@ -5,6 +5,10 @@ Check if the existing user has a session
 if it does
 destroy the session and redirect to login page
 */
+session_start();
+   unset($_SESSION["email"]);
+      unset($_SESSION["password"]);
+         
 }
 
-echo "HANDLE THIS PAGE";
+echo "logout successful";
