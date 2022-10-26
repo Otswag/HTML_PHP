3ebd702d3552c38fa07db5e11b08f2a83b8ac655
Otswag <106761383+Otswag@users.noreply.github.com>
2022-10-27 00:25:13 +0100

    Update register.php

--- php/register.php
+++ php/register.php
@@ -9,10 +9,30 @@ registerUser($username, $email, $password);
 }
 
 function registerUser($username, $email, $password){
-    //save data into the file
+    $form_data = array(
+        'fullname' => $username,
+        'email' => $email,
+        'password' => $password
+        );
     
-    // echo "OKAY";
+    if(checkUserExist($email)){
+        echo "User already exist";
+    }else{
+        $file = fopen('../storage/users.csv', 'a');
+        fputcsv($file, $form_data);
+        fclose($file);
+        echo "User registered successfully";
+    }
 }
-echo "HANDLE THIS PAGE";
 
-
+function checkUserExist($email){
+    $file = fopen('../storage/users.csv', 'r');
+    while(!feof($file){
+        $line = fgetcsv($file);
+        if($line[1] == $email){
+            return: true;
+        }
+    }
+          
+          return: false;
+}
